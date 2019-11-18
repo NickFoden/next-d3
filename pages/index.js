cac08ccdbede520aaf78111438b8aea1d13acdb3
Nick Foden <nicfoden@gmail.com>
2019-11-18 16:14:05 -0500

    Setting up

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Nav />
+
+    <div className="hero">
+      <h1 className="title">Next D3</h1>
+    </div>
+
+    <style jsx>{`
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title {
+        text-align: center;
+      }
+    `}</style>
+  </div>
+);
+
+export default Home;
